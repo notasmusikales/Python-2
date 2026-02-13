7003f27611db9dfc4ad91b0d9f9b080cdddca349
noel <noel@gmail.com>
2026-02-13 10:40:24 +0000

    avance

---
+++ 03-operadores/asignacion.py
@@ -0,0 +1,23 @@
+# Operadores de asignación
+edad = 30
+
+print("La edad es: ", edad)
+
+edad += 5 # edad = edad + 5
+
+print("La edad es sumando: ", edad)
+
+edad = edad + 2
+
+print("La edad es sumando de otra manera: ", edad)
+
+edad -= 2 # edad = edad - 5
+
+print("La edad es restando: ", edad)
+
+edad *= 2 # edad = edad * 2
+
+print("La edad es multiplicando: ", edad)
+
+
+print("La edad es: ", edad)
