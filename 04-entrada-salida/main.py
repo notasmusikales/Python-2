6c7a514380ced6c51b53f7fb35f1b8c4316e9f86
noel <noel@gmail.com>
2026-02-13 12:04:57 +0000

    commit 2

---
+++ 04-entrada-salida/main.py
@@ -0,0 +1,15 @@
+"""
+    Condicional IF en Python
+    
+    si se cumplen una condición hacer esto
+    
+    si no se cumple hacer esto otro
+    
+"""
+print("######### Ejemplo 1 ##########")
+color = input("Adivina mi color favorito: ")
+    
+if color == "azul":
+    print("Ese color es mi favorito")
+else:
+    print("Ese color no es mi favorito")
---
+++ 05-condicionales/main.py
@@ -0,0 +1,18 @@
+"""
+    Condicional IF en Python
+    
+    si se cumplen una condición hacer esto
+    
+    si no se cumple hacer esto otro
+    
+"""
+# Operadores de comparación
+print("######### Ejemplo 2 ##########")
+== igual a
+!= diferente a 
+> mayor que
+< menor que
+>= mayor o igual que
+<= menor o igual que    
+num1 = int(input("Ingrese un numero: "))
+num2 = int(input("Ingrese otro numero: "))
